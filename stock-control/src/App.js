1d22e5615a05f320d6885fb1a5d8ade1603974e9
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2019-05-30 23:22:59 -0300

    create Componente
    -navBar
    -sideBar
    -main

--- stock-control/src/App.js
+++ stock-control/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import Main from './coponents/main/main'
+import SideBar from './coponents/side-bar/side-bar'
+import NavBar from './coponents/nav-bar/nav-bar'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar></NavBar>
+      <SideBar></SideBar>
+      <Main></Main>
     </div>
   );
 }
---
+++ stock-control/src/coponents/side-bar/side-bar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class SideBar extends Component {
+  render() {
+    return (
+        <div>
+            test Side-Bar
+        </div>
+    );
+  }
+}
