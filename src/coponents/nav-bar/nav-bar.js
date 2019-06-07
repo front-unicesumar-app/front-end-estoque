33fa10c6a23e65f895bbcdfa874f060e1231ed40
zampieri <tzampieri7@gmail.com>
2019-06-07 20:59:36 -0300

    Remoção do material ui.

--- src/coponents/nav-bar/nav-bar.js
+++ src/coponents/nav-bar/nav-bar.js
@@ -28,15 +28,7 @@ function NavBar() {
     return (
       <div className={classes.root}>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
+        <p>AppBar works!</p>
       </AppBar>
     </div>
     );
