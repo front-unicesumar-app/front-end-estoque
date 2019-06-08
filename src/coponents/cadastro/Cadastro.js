50c96d8e5a5f9ee4db6f427247118a669d7ed03b
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2019-06-08 15:22:00 -0300

    Update tela de pedidos
    - Adicionado botão para adição e retirada de produto
    - Adicionado campo nome, número de equipes

--- src/coponents/cadastro/Cadastro.js
+++ src/coponents/cadastro/Cadastro.js
@@ -1,31 +1,55 @@
 import React from "react";
+import "./style.css";
 
 // import { Container } from './styles';
 
 export default function Cadastro() {
   return (
     <div className="row">
-      <div class="form-group col-5">
+      <div class="form-group col-8">
+        <label>Nome do Profesor</label>
+        <input type="text" class="form-control" placeholder="Professor" />
+      </div>
+      <div class="form-group col-4">
+        <label>Quantidade de Equipes</label>
+        <input type="text" class="form-control" placeholder="Equipes" />
+      </div>
+      <div className="col-12">
+        <div className="row">
+          <div className="col-10">
+            <p>Adicionar ou Excluir Campos</p>
+          </div>
+          <div className="col-2">
+            <button type="button" class="btn btn-primary btn-sm add">
+              +
+            </button>
+            <button type="button" class="btn btn-secondary btn-sm">
+              -
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="form-group col-6">
         <label>Nome do ingredite</label>
         <input type="text" class="form-control" placeholder="Ingrediente" />
       </div>
       <div class="form-group col-2">
         <label>Quantidade Grupo</label>
-        <input type="text" class="form-control" placeholder="Qtd."/>
+        <input type="text" class="form-control" placeholder="Qtd." />
       </div>
       <div class="form-group col-2">
         <label for="exampleFormControlSelect1">Und. de Medida</label>
         <select class="form-control" id="exampleFormControlSelect1">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
+          <option>g</option>
+          <option>Kg</option>
+          <option>Und.</option>
+          <option>L</option>
+          <option>ml</option>
         </select>
       </div>
       <div class="form-group col-2">
         <label>Total</label>
-        <input type="text" class="form-control" placeholder="Total"/>
+        <input type="text" class="form-control" placeholder="Total" />
       </div>
     </div>
   );
