a5cb159b38184fd1844521c0c5727a87a6083122
Edson Rodrigues <xpro23@gmail.com>
2019-06-12 21:52:45 -0300

    alteração

--- src/coponents/produtos/cadastrar-produtos/CadastrarProdutos.js
+++ src/coponents/produtos/cadastrar-produtos/CadastrarProdutos.js
@@ -3,6 +3,7 @@ import React from 'react';
 // import { Container } from './styles';
 
 export default function CadastrarProdutos() {
+  var a = 'a';
   return (
     <div>test Cadastrar Produtos</div>
   );
